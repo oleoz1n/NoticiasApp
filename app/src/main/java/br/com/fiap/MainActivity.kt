24505bf0b77aece548cb8f0500268237ccd2880c
oleoz1n <leopity0@gmail.com>
2024-11-01 12:42:59 -0300

    Feat: Make the views

--- app/src/main/java/br/com/fiap/MainActivity.kt
+++ app/src/main/java/br/com/fiap/MainActivity.kt
@@ -1,14 +1,21 @@
 package br.com.fiap
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import br.com.fiap.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy{
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(binding.root)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -16,6 +23,14 @@ class MainActivity : AppCompatActivity() {
                 v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
                 insets
         }
+
+        val btnAddNoticia = binding.btnAddNoticia
+
+        btnAddNoticia.setOnClickListener {
+            startActivity(Intent(this, PostAdd::class.java))
+            finish()
+        }
+
 
 
 
